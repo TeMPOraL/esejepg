9f46a5f2c053cabac7e03089e1e2c14f6d631a20
Jacek Zlydach <temporal.pl@gmail.com>
2012-11-25 03:55:33 +0100

    Listed all essays in comments

--- src/data/essays.lisp
+++ src/data/essays.lisp
@@ -1,11 +1,83 @@
-(defessay :judgement
-    :title "Dwa rodzaje osądów"
-    :url "/essays/judgement.html"
-    :orig-title "Two Kinds of Judgement"
-    :orig-url "http://paulgraham.com/judgement.html"
-    :translators '(:aajnoo)
-    :editors '(:temporal)
-    :disabled t)
+(defessay :progbot
+    :title "Programowanie metodą wstępującą (ang. bottom-up)"
+    :url "/essays/progbot.html"
+    :orig-title "Programming Bottom-Up"
+    :orig-url "http://paulgraham.com/progbot.html"
+    :date "???"
+    :orig-date "1993"
+    :translators '(:temporal)
+    :disabled t
+    :description "Taki sobie opis.")
+
+;; Chapter 2 of Ansi Common Lisp
+
+;; Chapter 1 of Ansi Common Lisp
+
+;; Lisp for Web-Based Applications
+
+;; Beating the Averages
+
+;; Java's Cover
+
+;; Being Popular
+
+;; Five Questions about Language Design
+
+;; The Roots of Lisp
+
+;; The Other Road Ahead
+
+;; What Made Lisp Different
+
+;; Why Arc Isn't Especially Object-Oriented
+
+;; Taste for Makers
+
+;; What Languages Fix
+
+;; Succinctness is Power
+
+;; Revenge of the Nerds
+
+;; A Plan for Spam
+
+;; Design and Research
+
+;; Better Bayesian Filtering
+
+;; Why Nerds are Unpopular
+
+;; The Hundred-Year Language
+
+;; If Lisp is So Great
+
+;; Hackers and Painters
+
+;; Filters that Fight Back
+
+;; What You Can't Say
+
+;; The Word "Hacker"
+
+;; How to Make Wealth
+
+;; Mind the Gap
+
+;; Great Hackers
+
+;; The Python Paradox
+
+;; The Age of the Essay
+
+;; What the Bubble Got Right
+
+;; A Version 1.0
+
+;; Bradley's Ghost
+
+;; It's Charisma, Stupid
+
+;; Made in USA
 
 (defessay :hs
     :title "[Tytuł będzie jak Asia zrobi]"
@@ -20,6 +92,12 @@
     :disabled t
     :part-of-hnp t)
 
+;; How to Start a Startup
+
+;; A Unified Theory of VC Suckage
+
+;; Undergraduation
+
 (defessay :writing44
     :title "O pisaniu, zwięźle"
     :url "/essays/writing44.html"
@@ -28,3 +106,224 @@
     :orig-url "http://paulgraham.com/writing44.html"
     :orig-date "Marzec 2005"
     :translators '(:aajnoo :temporal))
+
+;; Return of the Mac
+
+;; Why Smart People Have Bad Ideas
+
+;; The Submarine
+
+;; Hiring is Obsolete
+
+;; What Business Can Learn from Open Source
+
+;; After the Ladder
+
+;; Inequality and Risk
+
+;; What I Did this Summer
+
+;; Ideas for Startups
+
+;; The Venture Capital Squeeze
+
+;; How to Fund a Startup
+
+;; Web 2.0
+
+;; Good and Bad Procrastination
+
+;; How to Do What You Love
+
+;; Why YC
+
+;; 6,631,372
+
+;; Are Software Patents Evil?
+
+;; See Randomness
+
+;; The Hardest Lessons for Startups to Learn
+
+;; How to Be Silicon Valley
+
+;; Why Startups Condense in America
+
+;; The Power of the Marginal
+
+;; The Island Test
+
+;; Copy What You Like
+
+;; How to Present to Investors
+
+;; A Student's Guide to Startups
+
+;; The 18 Mistakes That Kill Startups
+
+;; How Art Can Be Good
+
+;; Learning from Founders
+
+;; Is It Worth Being Wise?
+
+;; Why to Not Not Start a Startup
+
+;; Microsoft is Dead
+
+(defessay :judgement
+    :title "Dwa rodzaje osądów"
+    :url "/essays/judgement.html"
+    :orig-title "Two Kinds of Judgement"
+    :orig-url "http://paulgraham.com/judgement.html"
+    :translators '(:aajnoo)
+    :editors '(:temporal)
+    :disabled t)
+
+;; The Hacker's Guide to Investors
+
+;; An Alternative Theory of Unions
+
+;; The Equity Equation
+
+;; Stuff
+
+;; Holding a Program in One's Head
+
+;; How Not to Die
+
+;; News from the Front
+
+;; How to Do Philosophy
+
+;; The Future of Web Startups
+
+;; Why to Move to a Startup Hub
+
+;; Six Principles for Making New Things
+
+;; Trolls
+
+;; A New Venture Animal
+
+;; You Weren't Meant to Have a Boss
+
+;; How to Disagree
+
+;; Some Heroes
+
+;; Why There Aren't More Googles
+
+;; Be Good
+
+;; Lies We Tell Kids
+
+;; Disconnecting Distraction
+
+;; Cities and Ambition
+
+;; The Pooled-Risk Company Management Company
+
+;; A Fundraising Survival Guide
+
+;; Why to Start a Startup in a Bad Economy
+
+;; The Other Half of "Artists Ship"  
+
+;; The High-Res Society
+
+;; Could VC be a Casualty of the Recession?
+
+;; After Credentials
+
+;; Keep Your Identity Small  
+
+;; Startups in 13 Sentences
+
+;; What I've Learned from Hacker News
+
+;; Can You Buy a Silicon Valley? Maybe.
+
+;; Why TV Lost
+
+;; How to Be an Angel Investor
+
+;; Relentlessly Resourceful
+
+;; Five Founders
+
+;; The Founder Visa
+
+;; A Local Revolution?
+
+;; Maker's Schedule, Manager's Schedule 
+
+;; Ramen Profitable
+
+;; The Trouble with the Segway
+
+;; What Kate Saw in Silicon Valley  
+
+;; The Anatomy of Determination 
+
+;; The List of N Things
+
+;; Post-Medium Publishing
+
+;; Persuade xor Discover 
+
+;; What Startups Are Really Like
+
+;; Apple's Mistake
+
+;; Organic Startup Ideas
+
+;; How to Lose Time and Money 
+
+;; The Top Idea in Your Mind 
+
+;; The Acceleration of Addictiveness
+
+;; The Future of Startup Funding 
+
+;; What Happened to Yahoo 
+
+;; High Resolution Fundraising 
+
+;; Where to See Silicon Valley
+
+;; The New Funding Landscape
+
+;; What We Look for in Founders
+
+;; Tablets
+
+;; Founder Control
+
+;; Subject: Airbnb
+
+;; The Patent Pledge
+
+;; Why Startup Hubs Work
+
+;; Snapshot: Viaweb, June 1998
+
+;; Schlep Blindness
+
+;; A Word to the Resourceful
+
+;; Frighteningly Ambitious Startup Ideas
+
+;; Defining Property
+
+;; Writing and Speaking
+
+;; The Top of My Todo List
+
+;; Black Swan Farming
+
+;; Startup = Growth
+
+;; The Hardware Renaissance
+
+;; How to Get Startup Ideas
