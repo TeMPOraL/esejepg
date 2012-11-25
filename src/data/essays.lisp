abf184d999da06653438afb153b3eab423dad4d6
Jacek Zlydach <temporal.pl@gmail.com>
2012-11-25 03:06:13 +0100

    Loads of changes related to centralizing essay database.

---
+++ src/data/essays.lisp
@@ -0,0 +1,30 @@
+(defessay :judgement
+    :title "Dwa rodzaje osądów"
+    :url "/essays/judgement.html"
+    :orig-title "Two Kinds of Judgement"
+    :orig-url "http://paulgraham.com/judgement.html"
+    :translators '(:aajnoo)
+    :editors '(:temporal)
+    :disabled t)
+
+(defessay :hs
+    :title "[Tytuł będzie jak Asia zrobi]"
+    :url "/essays/hs.html"
+    :date "Grudzień 2012"
+    :orig-title "What You'll Wish You'd Known"
+    :orig-url "http://paulgraham.com/hs.html"
+    :orig-date "Kwiecień 2007"
+    :alt-translations '(("http://T1" "T1") ("http://T2" "T2") ("http://T3" "T3"))
+    :translators '(:aajnoo)
+    :editors '(:temporal)
+    :disabled t
+    :part-of-hnp t)
+
+(defessay :writing44
+    :title "O pisaniu, zwięźle"
+    :url "/essays/writing44.html"
+    :date "Listopad 2012"
+    :orig-title "Writing, Briefly"
+    :orig-url "http://paulgraham.com/writing44.html"
+    :orig-date "Marzec 2005"
+    :translators '(:aajnoo :temporal))
