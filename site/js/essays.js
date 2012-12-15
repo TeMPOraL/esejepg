2df2e73b3436980fc9336af8e875fbcf410d107f
Jacek Zlydach <temporal.pl@gmail.com>
2012-12-15 16:55:01 +0100

    paragraph links - some more changes

--- site/js/essays.js
+++ site/js/essays.js
@@ -2,7 +2,15 @@ var Essays = {
 
     addParagraphLinks : function() {
         jQuery("#content p").each(function(idx, el) {
-            el.innerHTML = "<a href='#" + el.id + "' name='" + el.id + "'>&para;</a>" + el.innerHTML;
+            var link = document.createElement("a");
+            link.setAttribute("class", "paragraph-link");
+            link.setAttribute("name", el.id);
+            link.setAttribute("href", "#" + el.id);
+            link.innerHTML = "&para;";
+
+            el.insertBefore(link, el.firstChild);
+            el.addEventListener("mouseover", function() { link.style.color = "#AAAAAA"; }, false);
+            el.addEventListener("mouseout", function() { link.style.color = "#FFFFFF"; }, false);
         });
     }
 };
