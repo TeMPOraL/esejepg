701cca28ea22586418f218a080db0e7660393202
Jacek Zlydach <temporal.pl@gmail.com>
2012-12-15 16:32:53 +0100

    more js fix

--- site/js/essays.js
+++ site/js/essays.js
@@ -2,9 +2,9 @@ var Essays = {
 
     addParagraphLinks : function() {
         jQuery("#content p").each(function(idx, el) {
-            el.prepend("<a href='#'" + el.id + "' name='" + el.id + "'>&para;</a>")});
+            el.innerHTML = "<a href='#'" + el.id + "' name='" + el.id + "'>&para;</a>" + el.innerHTML;
+        });
     }
-
 };
 
 jQuery(document).ready(function() {
